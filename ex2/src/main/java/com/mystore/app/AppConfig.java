78037e07ce073167bc6429d65b179a94a05718dd
Vedant Sagare <sagarevedant@gmail.com>
2025-02-11 22:55:08 +0530

    Exercise2

--- ex2/src/main/java/com/mystore/app/AppConfig.java
+++ ex2/src/main/java/com/mystore/app/AppConfig.java
@@ -1,5 +1,23 @@
 package com.mystore.app;
 
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class AppConfig {
+	@Bean
+    public Product product1() {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Tumbler");
+        return product;
+    }
+
+    @Bean
+    public Product product2() {
+        Product product = new Product();
+        product.setId(2);
+        product.setName("Bucket");
+        return product;
+    }
 }
